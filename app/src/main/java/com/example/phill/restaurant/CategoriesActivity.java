f31bb43bcc431ec175c63e7d79bdc9f7dd59b39a
Philline <fidik@live.nl>
2018-12-01 15:56:24 +0100

    Initial Project

--- app/src/main/java/com/example/phill/restaurant/CategoriesActivity.java
+++ app/src/main/java/com/example/phill/restaurant/CategoriesActivity.java
@@ -1,15 +1,19 @@
 package com.example.phill.restaurant;
 
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class CategoriesActivity extends AppCompatActivity  implements CategoriesRequest.Callback {
-
+public class CategoriesActivity extends AppCompatActivity  implements CategoriesRequest.Callback, AdapterView.OnItemClickListener{
 
 
     @Override
@@ -29,9 +33,7 @@ public class CategoriesActivity extends AppCompatActivity  implements Categories
 //        listView.setOnItemClickListener(new GridItemClickListener());
 
 
-
     }
-
 
     @Override
     public void  gotCategories(ArrayList<String> categories) {
@@ -39,12 +41,20 @@ public class CategoriesActivity extends AppCompatActivity  implements Categories
 //            CategoriesActivity catergorie = new CategoriesActivity(String);
 //            categories.add(String.valueOf(catergorie));
         Toast.makeText(this, categories.get(0), Toast.LENGTH_LONG).show();
-
     }
 
     @Override
     public void gotCategoriesError(String message) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+//        MenuActivity by way of an intent, making sure that you provide via putExtra a string with the category name
+        CategoriesRequest categori = (CategoriesRequest) parent.getItemAtPosition(position);
+        Intent intent = new Intent(this, MenuActivity.class);
+        intent.putExtra("categorie", (Parcelable) categori);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/phill/restaurant/MenuRequest.java
+++ app/src/main/java/com/example/phill/restaurant/MenuRequest.java
@@ -1,11 +1,75 @@
 package com.example.phill.restaurant;
 
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 //will retrieve the menu items for this category
-public class MenuRequest {
+public class MenuRequest implements Response.Listener<JSONObject>, Response.ErrorListener {
 
+    private Context Context2 ;
     private String url = "https://resto.mprog.nl/menu";
+    private Callback activity;
+    ArrayList<String> MenuItem = new ArrayList<String>();
 
+    MenuRequest(Context context) {
+//        super(context);
+        this.Context2 = context;
+//        this.activity = activity;
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        activity.gotMenuError(error.getMessage());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        try {
+            for(int i=0;i<response.length();i++){
+                JSONObject jresponse = response.getJSONObject(String.valueOf(i));
+                String menui = jresponse.getString(String.valueOf(i));
+
+                // add catergorie to Arraylist
+                MenuItem.add(menui);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //    attempt to retrieve the categories from the API
+    public void getMenu(Callback activity){
+
+        // use Volley to create a new RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(Context2);
+
+        // create a JsonObjectRequest
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, this, this);
+        queue.add(jsonObjectRequest);
+
+        // pass the arrylist back
+        activity.gotMenu(MenuItem);
+
+    }
+
+    public interface Callback {
+        void gotMenu(ArrayList<String> categories);
+        void gotMenuError(String message);
+    }
 
 
 }
