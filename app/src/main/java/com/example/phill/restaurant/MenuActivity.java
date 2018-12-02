e5e28a243f27868d222392fe83141b1801c04b66
Philline <fidik@live.nl>
2018-12-02 18:36:39 +0100

    Initial Project

--- app/src/main/java/com/example/phill/restaurant/MenuActivity.java
+++ app/src/main/java/com/example/phill/restaurant/MenuActivity.java
@@ -3,12 +3,14 @@ package com.example.phill.restaurant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 //In your MenuActivity’s onCreate, retrieve the category string and make sure that this string is part of the request for menu items.
-class MenuActivity implements MenuRequest.Callback{
+public class MenuActivity extends AppCompatActivity implements MenuRequest.Callback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,20 @@ class MenuActivity implements MenuRequest.Callback{
         Intent intent = getIntent();
         String message = intent.getStringExtra("categorie");
 
+        // get the menu
+        MenuRequest request = new MenuRequest(this);
+        request.getMenu(this);
+
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
     }
 
     @Override
-    public void gotMenu(ArrayList<String> categories) {
+    public void gotMenu(ArrayList<MenuItem> categories) {
 //        custom view for the adapter, which will display as much info about the menu items as possible, including a picture!
+        MenuAdapter adapter = new MenuAdapter(this,R.layout.categories_activity,MenuItem);
+
     }
 
     @Override
