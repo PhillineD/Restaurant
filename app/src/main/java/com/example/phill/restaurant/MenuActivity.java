1b3778caf2a49bf739ab7b2286a31470dcc7ecc5
Philline <fidik@live.nl>
2018-12-02 18:54:08 +0100

    Initial Project

--- app/src/main/java/com/example/phill/restaurant/MenuActivity.java
+++ app/src/main/java/com/example/phill/restaurant/MenuActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.MenuAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,12 +37,26 @@ public class MenuActivity extends AppCompatActivity implements MenuRequest.Callb
     @Override
     public void gotMenu(ArrayList<MenuItem> categories) {
 //        custom view for the adapter, which will display as much info about the menu items as possible, including a picture!
-        MenuAdapter adapter = new MenuAdapter(this,R.layout.categories_activity,MenuItem);
+        MenuAdapter adapter = new MenuAdapter(this,R.layout.categories_activity, categories);
+        ListView listView = findViewById();
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MenuItem ChooseItems = (MenuItem) parent.getItemAtPosition(position);
+                Intent item = new Intent(getApplicationContext(),MenuItemActivity.class);
+                item.putExtra("name", ChooseItems.getName());
+                item.putExtra("price", ChooseItems.getPrice());
+                item.putExtra("description", ChooseItems.getDescription());
+                // image moet nog
+                startActivity(item);
+            }
+        });
 
     }
 
     @Override
     public void gotMenuError(String message) {
-
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
